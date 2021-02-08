dba25decf26a36323eff36480c3057c23e0598c0
Madeline Liao <madeline.m.liao@gmail.com>
2021-02-08 13:52:46 -0800

    merge fix cnn.py

--- src/classification/models/cnn.py
+++ src/classification/models/cnn.py
@@ -57,8 +57,43 @@ class CNNClassifier(nn.Module):
 
         return logits, probas
 
+class TRECNNClassifier(nn.Module):
+    def __init__(self, config, layer_size=64, num_channels=1, keep_prob=1.0, image_size=28):
+        super(CNNClassifier, self).__init__()
+        
+        self.m = self.config.tre.m
+        self.p = self.config.tre.p
 
-<<<<<<< Updated upstream
+        self.layer1 = convLayer(num_channels, layer_size, keep_prob)
+        self.layer2 = convLayer(layer_size, layer_size, keep_prob)
+        self.layer3 = convLayer(layer_size, layer_size, keep_prob)
+        self.layer4 = convLayer(layer_size, layer_size, keep_prob)
+
+        finalSize = int(math.floor(image_size / (2 * 2 * 2 * 2)))
+        self.outSize = finalSize * finalSize * layer_size
+        self.fc_list = [nn.Linear(self.outSize, 1622) for _ in range(self.m)]
+    
+    def forward(self, image_input):
+        """
+        Use CNN defined above
+        :param image_input:
+        :return:
+        """
+        x = self.layer1(image_input)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = x.view(self.m, x.size()[0], -1) # TODO
+        
+        logits = []
+        for x_i, fc in zip(x, self.fc_list):
+            logits.append(fc(x_i))
+
+        logits = torch.stack(logits)
+        probas = F.softmax(logits, dim=-1)
+
+        return logits, probas
 class BinaryCNNClassifier(nn.Module):
     def __init__(self, layer_size=64, num_channels=1, keep_prob=1.0, image_size=28):
         super(BinaryCNNClassifier, self).__init__()
@@ -69,15 +104,6 @@ class BinaryCNNClassifier(nn.Module):
         :param keep_prob:
         :param image_size:
         """
-=======
-class TRECNNClassifier(nn.Module):
-    def __init__(self, config, layer_size=64, num_channels=1, keep_prob=1.0, image_size=28):
-        super(CNNClassifier, self).__init__()
-        
-        self.m = self.config.tre.m
-        self.p = self.config.tre.p
-
->>>>>>> Stashed changes
         self.layer1 = convLayer(num_channels, layer_size, keep_prob)
         self.layer2 = convLayer(layer_size, layer_size, keep_prob)
         self.layer3 = convLayer(layer_size, layer_size, keep_prob)
@@ -86,11 +112,7 @@ class TRECNNClassifier(nn.Module):
         finalSize = int(math.floor(image_size / (2 * 2 * 2 * 2)))
         self.outSize = finalSize * finalSize * layer_size
 
-<<<<<<< Updated upstream
         self.fc = nn.Linear(self.outSize, 1)
-=======
-        self.fc_list = [nn.Linear(self.outSize, 1622) for _ in range(self.m)]
->>>>>>> Stashed changes
 
     def forward(self, image_input):
         """
@@ -98,28 +120,13 @@ class TRECNNClassifier(nn.Module):
         :param image_input:
         :return:
         """
-<<<<<<< Updated upstream
-=======
-        # x = x.view(-1, ) TODO
->>>>>>> Stashed changes
         x = self.layer1(image_input)
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
-<<<<<<< Updated upstream
         x = x.view(x.size()[0], -1)
 
         logits = self.fc(x)
         probas = torch.sigmoid(logits)
-=======
-        x = x.view(self.m, x.size()[0], -1) # TODO
-        
-        logits = []
-        for x_i, fc in zip(x, self.fc_list):
-            logits.append(fc(x_i))
-
-        logits = torch.stack(logits)
-        probas = F.softmax(logits, dim=-1)
->>>>>>> Stashed changes
 
         return logits, probas
